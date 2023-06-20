3c3dde11e4f20ea262703a1af795eba0c1cce90c
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-06-20 15:29:51 +0300

    header + logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,25 +14,19 @@ import {
 } from './Header.styled';
 import coin from './1.png';
 import logout from './logout2x.png';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
 import MediaQuery from 'react-responsive';
+import { useDispatch, useSelector } from 'react-redux';
+import { userLogout } from 'store/authOperation';
 
 function Header() {
-  // const isAuth = useSelector();
-  const email = 'admin@admin.com';
-  const login = email.substring(0, email.indexOf('@'));
-  const firstLetter = login.charAt(0).toUpperCase();
-
-  // const { user, token } = useSelector(state => state.auth);
-  // const dispatch = useDispatch();
-  // const navigate = useNavigate();
+  const accessToken = useSelector(state => state.auth.accessToken);
+  const email = useSelector(state => state.auth.user.email);
+  const login = email ? email.substring(0, email.indexOf('@')) : '';
+  const firstLetter = login ? login.charAt(0).toUpperCase() : '';
+  const dispatch = useDispatch();
 
   function logoutFunc() {
-    //   dispatch(actions.logout());
-    //   deleteToken();
-    //   navigate('/');
+    dispatch(userLogout());
   }
 
   return (
@@ -47,22 +41,23 @@ function Header() {
             ss
           </Logo>
         </NLink>
-        {/* {isAuth && 'вміст сторінки нижче'} */}
-        <UserInfo>
-          <FirstLetter>{firstLetter}</FirstLetter>
-          <Login>{login}</Login>
-          <MediaQuery minWidth={768}>
-            <Line></Line>
-            <ExitBtn onClick={logoutFunc} type="button">
-              Exit
-            </ExitBtn>
-          </MediaQuery>
-          <MediaQuery maxWidth={767}>
-            <LogoutBtn onClick={logoutFunc} type="button">
-              <LogoutImg src={logout} alt="logoutIcon" />
-            </LogoutBtn>
-          </MediaQuery>
-        </UserInfo>
+        {accessToken && (
+          <UserInfo>
+            <FirstLetter>{firstLetter}</FirstLetter>
+            <Login>{login}</Login>
+            <MediaQuery minWidth={768}>
+              <Line></Line>
+              <ExitBtn onClick={logoutFunc} type="button">
+                Exit
+              </ExitBtn>
+            </MediaQuery>
+            <MediaQuery maxWidth={767}>
+              <LogoutBtn onClick={logoutFunc} type="button">
+                <LogoutImg src={logout} alt="logoutIcon" />
+              </LogoutBtn>
+            </MediaQuery>
+          </UserInfo>
+        )}
       </Nav>
     </BodyHeader>
   );
