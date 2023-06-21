80b239f3eb3506fa108f8a4a944f16969c68a6ac
m1lky4 <temanafanin1337@gmail.com>
2023-06-21 19:52:22 +0200

    finished expenses summary and all styles

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -6,31 +6,32 @@ import {
 } from './Summary.styled';
 import { SummaryTitle } from './Summary.styled';
 import { SummaryList } from './Summary.styled';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getTransactionExpense,
+  // getTransactionIncome,
+} from 'store/transactionsOperations';
 export const Summary = () => {
-  const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
+  const monthStats = useSelector(state => state.transaction.monthStatsExpenses);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getTransactionExpense());
+    // dispatch();
+  }, []);
+
   return (
     <SummaryWrapper>
       <SummaryTitle>Summary</SummaryTitle>
       <SummaryList>
-        {months.map(month => {
-          return (
+        {Object.entries(monthStats).map(month => {
+          return month[1] !== 'N/A' ? (
             <SummaryListItem>
-              <SummaryText>{month}</SummaryText>
-              <SummaryAmount>10000.00</SummaryAmount>
+              <SummaryText>{month[0]}</SummaryText>
+              <SummaryAmount>{month[1]}</SummaryAmount>
             </SummaryListItem>
+          ) : (
+            <SummaryListItem></SummaryListItem>
           );
         })}
       </SummaryList>
