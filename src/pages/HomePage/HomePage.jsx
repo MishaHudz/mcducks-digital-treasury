62501a5d0fe3d65e1040d484284f11e282c94836
m1lky4 <temanafanin1337@gmail.com>
2023-06-21 20:04:02 +0200

    deleted home page functions

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,44 +1,19 @@
 import ReportsLink from 'components/ReportsLink/ReportsLink';
 import { Summary } from 'components/Summary/Summary';
 import { Addtransaction } from 'components/AddTransaction/AddTransaction';
-import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import TransactionTable from 'components/TransactionTable/TransactionTab';
 
 function HomePage() {
-  const [date, setDate] = useState('');
-  const [year, setYear] = useState(null);
   const { accessToken } = useSelector(state => state.auth);
 
   const navigate = useNavigate();
-  const day = new Date();
-  const startDate = `${day.getDate()}.${
-    day.getMonth() + 1
-  }.${day.getFullYear()}`;
-  useEffect(() => {
-    !accessToken && navigate('/authorization');
-  }, [accessToken, navigate]);
-
-  useEffect(() => {
-    setDate(startDate);
-    setYear(startDate.split('.')[2]);
-  }, []);
-  const getTransactionsMonthYear = (month, year) => async () => {
-    try {
-      const response = await getTransactionsByPeriod(`${month}-${year}`);
-      console.log(response);
-    } catch ({ response }) {
-      console.log(response);
-    }
-  };
   return (
     <>
-      {console.log(startDate)}
-      {/* {getTransactionsMonthYear(year)} */}
       <ReportsLink />
       <div>Home Page</div>
-      <Summary year={year}></Summary>
+      <Summary />
       <Addtransaction />
       <TransactionTable />
     </>
