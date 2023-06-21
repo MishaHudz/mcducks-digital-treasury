e3c3a5fdc9f59f9b5f85745903118fda249297f2
Stark561 <123174593+Stark561@users.noreply.github.com>
2023-06-21 16:53:00 +0300

    fix BalanceCenter

--- src/components/BalanceCenter/BalanceCenter.jsx
+++ src/components/BalanceCenter/BalanceCenter.jsx
@@ -5,22 +5,12 @@ import BalanceBtn from './BalanceBtn';
 import BalanceModal from '../BalanceModal/BalanceModal';
 import { Forma, Title, Data } from './BalanceCenter.styled';
 
-// const formBalanc = value => {
-
-// };
-
 const BalanceCenter = () => {
-  // const initialState = useSelector(Users.balance);
-//   const newBalance = useSelector();
-//   const balance = newBalance === 0 ? initialState : newBalance;
-  // const [input, setInput] = useState(
-  //   `${formBalanc(initialState.toFixed(2))} UAH`
-  // );
 
-  // const handleChange = e => {
-  //   setInput(e.target.value);
-  // };
+  const initialState = useSelector(store => store.auth.balance);
+  const [input, setInput] = useState(0);
 
+console.log(initialState);
   return (
     <Forma>
       <Title>Balance:</Title>
@@ -28,17 +18,17 @@ const BalanceCenter = () => {
         name="balanceCenter"
         title="Field must contain only numbers"
         type="number"
-        // value={input}
-        // onChange={handleChange}
-        // disabled={initialState === 0 ? false : true}
-        // placeholder={
-        //   initialState === 0
-        //     ? `00.00 UAH`
-        //     : `${formBalanc(balance.toFixed(2))} UAH`
-        // }
+        value={initialState === 0 ? input : `${initialState.toFixed(2)} UAH`}  
+        onChange={setInput(e.target.value)}
+        disabled={initialState === 0 ? false : true}
+        placeholder={
+          initialState === 0
+            ? `00.00 UAH`
+            : `${formBalanc(balance.toFixed(2))} UAH`
+        }
       />
-      {/* {!initialState && <BalanceBtn balanceToUpdate={input} />}
-      {!initialState && <BalanceModal />} */}
+      {initialState === 0 && <BalanceBtn balanceToUpdate={input} />}
+      {!initialState && <BalanceModal />}
     </Forma>
   );
 };
