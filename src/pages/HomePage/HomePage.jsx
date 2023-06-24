b636e98bca102cefd69181b2f56c7678af104b3d
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-06-24 15:05:31 +0300

    !

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -89,11 +89,6 @@ function HomePage() {
                 <BalanceModal />
                 <ReportsLink />
               </TestBox>
-            </MediaQuery>
-
-            <ExpenseIncomeBtn />
-
-            <MediaQuery minWidth={768}>
               <ExpenseIncomeBtn />
             </MediaQuery>
             <OperationsBox>
