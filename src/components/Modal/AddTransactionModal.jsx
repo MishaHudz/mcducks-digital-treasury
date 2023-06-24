46ea3abf2a96403118c6185c5f2bfcf5e92fe7c8
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-06-24 19:18:17 +0300

    modal add transaction

--- src/components/Modal/AddTransactionModal.jsx
+++ src/components/Modal/AddTransactionModal.jsx
@@ -1,7 +1,10 @@
 import Modal from 'react-modal';
-import { BoxButton, ExitModal } from './AddTransactionModal.styled';
+import {
+  BoxButton,
+  CloseModalAddTransaction,
+  ExitModal,
+} from './AddTransactionModal.styled';
 import { Addtransaction } from 'components/AddTransaction/AddTransaction';
-import { GoToMain } from 'components/goBack/goToMain';
 
 Modal.setAppElement('#root');
 
@@ -12,23 +15,51 @@ function AddTransactionModal({
   setStartDate,
 }) {
   return (
-    <ExitModal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      contentLabel="Exit Confirmation"
-      style={{
-        overlay: {
-          background: 'rgba(255, 255, 255, 0.09)',
-          boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
-          backdropFilter: 'blur(1.2px)',
-        },
-      }}
-    >
-      <GoToMain />
-      <BoxButton>
-        <Addtransaction isOpen={isOpen} mobStartDate={startDate} />
-      </BoxButton>
-    </ExitModal>
+    <>
+      {isOpen && (
+        <CloseModalAddTransaction onClick={onRequestClose}>
+          <svg
+            style={{ marginRight: '0px' }}
+            xmlns="http://www.w3.org/2000/svg"
+            width={'18px'}
+            height={'18px'}
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <g id="keyboard_backspace-24px 1" clipPath="url(#clip0_15_1344)">
+              <path
+                id="Vector"
+                d="M21 11H6.83L10.41 7.41L9 6L3 12L9 18L10.41 16.59L6.83 13H21V11Z"
+                fill="#F6F7FB"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_15_1344">
+                <rect width={'18px'} height={'18px'} fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          to transaction
+        </CloseModalAddTransaction>
+      )}
+
+      <ExitModal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        contentLabel="Exit Confirmation"
+        style={{
+          overlay: {
+            background: 'rgba(255, 255, 255, 0.09)',
+            boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)',
+            backdropFilter: 'blur(1.2px)',
+          },
+        }}
+      >
+        <BoxButton>
+          <Addtransaction isOpen={isOpen} mobStartDate={startDate} />
+        </BoxButton>
+      </ExitModal>
+    </>
   );
 }
 
